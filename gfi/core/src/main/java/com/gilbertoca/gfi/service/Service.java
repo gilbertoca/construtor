206d3f6f9af7077cc09443446373b4c69039395a
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2008-02-01 21:12:04 +0000

    ajustes.
    git-svn-id: https://construtor.googlecode.com/svn/trunk@252 daf9df78-7b24-0410-bc62-633826f6319b

--- gfi/core/src/main/java/com/gilbertoca/gfi/service/Service.java
+++ gfi/core/src/main/java/com/gilbertoca/gfi/service/Service.java
@@ -6,25 +6,52 @@ package com.gilbertoca.gfi.service;
 
 import java.io.Serializable;
 import java.util.Collection;
+
 import net.sourceforge.orbroker.Broker;
+import net.sourceforge.orbroker.Query;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- *
+ * 
  * @author gilberto
  */
 abstract public class Service<T, PK extends Serializable> {
-    protected transient final Log log = LogFactory.getLog(getClass());
-    abstract public Broker getBroker();
-    protected Broker getBroker(String brokerName) {
-        return ResourceLocator.getInstance().getBroker(brokerName);
-    }
-    abstract public Collection<T> findAll();
-    abstract public T findByPk(PK pk);
-    abstract public void insert(T entity);
-    abstract public void update(T entity);
-    abstract public void deleteAll();
-    abstract public void deleteByPk(PK pk);
-    abstract public Collection<T> findByNamedQuery(String queryName);
+	protected transient final Log log = LogFactory.getLog(getClass());
+
+	abstract public Broker getBroker();
+
+	abstract public Collection<T> findAll();
+
+	abstract public T findByPk(PK pk);
+
+	protected boolean findByPk(T entity) {
+		Query qry = getBroker().startQuery();
+		if (entity == null) {
+			throw new IllegalArgumentException("Entidade n�o pode ser nulo!");
+		}
+		qry.setParameter(entity.getClass().getSimpleName().toLowerCase(),
+				entity);
+		try {
+			return qry.selectOne("get" + entity.getClass().getSimpleName()
+					+ "ByPk", entity);
+		} finally {
+			qry.close();
+		}
+	}
+
+	abstract public void insert(T entity);
+
+	abstract public void update(T entity);
+
+	abstract public void deleteAll();
+
+	abstract public void deleteByPk(PK pk);
+
+	abstract public Collection<T> findByNamedQuery(String queryName);
+
+	protected Broker getBroker(String brokerName) {
+		return ResourceLocator.getInstance().getBroker(brokerName);
+	}
 }
