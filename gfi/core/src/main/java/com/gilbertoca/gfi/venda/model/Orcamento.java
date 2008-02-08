b162e5127d11063858c85bf5b91509a641a33e8b
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2008-02-08 22:01:16 +0000

    Apenas uma correção de diretorios.
    git-svn-id: https://construtor.googlecode.com/svn/trunk@271 daf9df78-7b24-0410-bc62-633826f6319b

---
+++ gfi/core/src/main/java/com/gilbertoca/gfi/venda/model/Orcamento.java
@@ -0,0 +1,428 @@
+package com.gilbertoca.gfi.venda.model;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+ * @author Gilberto
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * @hibernate.class table = "construtor.venda_orcamento"
+ */
+public class Orcamento implements Serializable{
+    private Integer cdCliente;
+    private Cliente cliente;    
+    private Integer cdFuncionario;
+    private Integer cdOrcamento;
+    private Integer cdPlanoPagamento;
+    private Date dtOrcamento;
+    private Date dtValidadeOrcamento;
+    /**
+     * Tipo pagamento na venda: Prazo ou Vista
+     */
+    private String tipoPagamento = "Vista";
+    /**
+     * Forma pagamento na venda:
+     * D-Dinheiro; H-Cheque; P-Cheque-pré-datado;
+     * C-Cartao de Credito; U-Dollar; T-Ticket/Vale;
+     * B-Boleto Bancario; L-Duplicata ou O-Convenio.
+     */
+    private String formaPagamento = "D";	
+    private String status;
+    private String NomeCliente;
+    private String observacao;
+    private String tipoCliente;//Sendo:F=fornecedor C=cliente
+    private Double valorDesconto;
+    private Double valorEntrada;
+    private Double valorParcela;
+    private Double valorTotal;
+    private Double valorTotalPrazo;
+    private Double valorTotalProduto;
+    private Boolean flProcessado = new Boolean(false);
+    private Set orcamentoItems = Collections.EMPTY_SET;
+
+
+    /**
+     * 
+     */
+    public Orcamento() {
+
+    }
+
+    /**
+     * @hibernate.property column="cd_cliente"
+     * @return Returns the cdCliente.
+     */
+    public Integer getCdCliente() {
+        return cdCliente;
+    }
+
+    /**
+     * @hibernate.property  column="fl_processado" length="1"
+     */
+     public Boolean getFlProcessado() {
+         return flProcessado;
+     }
+
+    /**
+     * @param b
+     */
+    public void setFlProcessado(Boolean flProcessado) {
+        this.flProcessado  = flProcessado;
+    }    
+    /**
+     * @hibernate.property column="cd_funcionario"
+     * @return Returns the cdFuncionario.
+     */
+    public Integer getCdFuncionario() {
+        return cdFuncionario;
+    }
+    /**
+     * @hibernate.id generator-class="sequence" column="cd_orcamento" unsaved-value = "null"
+     * @hibernate.generator-param name = "sequence" value = "construtor.venda_orcamento_sequence"
+     * @return Integer
+     */
+    public Integer getCdOrcamento() {
+        return cdOrcamento;
+    }
+    
+    /**
+     * @hibernate.many-to-one insert="false" update="false" 
+     * cascade="none" column="cd_cliente" outer-join="true"
+     * @return a populated cliente object (based on the cdCliente)
+     */
+    public Cliente getCliente() { 
+        return this.cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }    
+	
+    /**
+     * @hibernate.property column="forma_pagamento" length = "1"
+     * @return Returns the formaPagamento.
+     */
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+	
+    public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+	
+
+	/**
+     * @hibernate.property column="cd_plano_pagamento"
+     * @return Returns the cdPlanoPagamento.
+     */
+    public Integer getCdPlanoPagamento() {
+        return cdPlanoPagamento;
+    }
+    /**
+     * @hibernate.property column="dt_orcamento"
+     * @return Returns the dtOrcamento.
+     */
+    public Date getDtOrcamento() {
+        return dtOrcamento;
+    }
+    /**
+     * @hibernate.property column="dt_validade_orcamento"
+     * @return Returns the dtValidadeOrcamento.
+     */
+    public Date getDtValidadeOrcamento() {
+        return dtValidadeOrcamento;
+    }
+    /**
+     * @hibernate.property column="tipo_pagamento" length = "10"
+     * @return Returns the tipoPagamento.
+     */
+    public String getTipoPagamento() {
+        return tipoPagamento;
+    }
+    /**
+     * @hibernate.property column="status" length = "1"
+     * @return Returns the status.
+     */
+    public String getStatus() {
+        return status;
+    }
+    /**
+     * @hibernate.property column="nome_cliente" length = "60"
+     * @return Returns the nomeCliente.
+     */
+    public String getNomeCliente() {
+        return NomeCliente;
+    }
+    /**
+     * @hibernate.property column="observacao" length = "255"
+     * @return Returns the observacao.
+     */
+    public String getObservacao() {
+        return observacao;
+    }
+    /**
+     * @hibernate.property column="tipo_cliente" length = "1"
+     * @return Returns the tipoCliente.
+     */
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+    /**
+     * @hibernate.property column="valor_desconto"
+     * @return Returns the valorDesconto.
+     */
+    public Double getValorDesconto() {
+        return valorDesconto;
+    }
+    /**
+     * @hibernate.property column="valor_entrada"
+     * @return Returns the valorEntrada.
+     */
+    public Double getValorEntrada() {
+        return valorEntrada;
+    }
+    /**
+     * @hibernate.property column="valor_parcela"
+     * @return Returns the valorParcela.
+     */
+    public Double getValorParcela() {
+        return valorParcela;
+    }
+    /**
+     * @hibernate.property column="valor_total"
+     * @return Returns the valorTotal.
+     */
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+    /**
+     * @hibernate.property column="valor_total_prazo"
+     * @return Returns the valorTotalPrazo.
+     */
+    public Double getValorTotalPrazo() {
+        return valorTotalPrazo;
+    }
+    /**
+     * @hibernate.property column="valor_total_produto"
+     * @return Returns the valorTotalProduto.
+     */
+    public Double getValorTotalProduto() {
+        return valorTotalProduto;
+    }
+
+    /**
+     * @param cdCliente The cdCliente to set.
+     */
+    public void setCdCliente(Integer cdCliente) {
+        this.cdCliente = cdCliente;
+    }
+    /**
+     * @param cdFuncionario The cdFuncionario to set.
+     */
+    public void setCdFuncionario(Integer cdFuncionario) {
+        this.cdFuncionario = cdFuncionario;
+    }
+    /**
+     * @param cdOrcamento The cdOrcamento to set.
+     */
+    public void setCdOrcamento(Integer cdOrcamento) {
+        this.cdOrcamento = cdOrcamento;
+    }
+    /**
+     * @param cdPlanoPagamento The cdPlanoPagamento to set.
+     */
+    public void setCdPlanoPagamento(Integer cdPlanoPagamento) {
+        this.cdPlanoPagamento = cdPlanoPagamento;
+    }
+    /**
+     * @param dtOrcamento The dtOrcamento to set.
+     */
+    public void setDtOrcamento(Date dtOrcamento) {
+        this.dtOrcamento = dtOrcamento;
+    }
+    /**
+     * @param dtValidadeOrcamento The dtValidadeOrcamento to set.
+     */
+    public void setDtValidadeOrcamento(Date dtValidadeOrcamento) {
+        this.dtValidadeOrcamento = dtValidadeOrcamento;
+    }
+    /**
+     * @param tipoPagamento The tipoPagamento to set.
+     */
+    public void setTipoPagamento(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+    /**
+     * @param status The status to set.
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    /**
+     * @param nomeCliente The nomeCliente to set.
+     */
+    public void setNomeCliente(String nomeCliente) {
+        NomeCliente = nomeCliente;
+    }
+    /**
+     * @param observacao The observacao to set.
+     */
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    /**
+     * @param tipoCliente The tipoCliente to set.
+     */
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+    /**
+     * @param valorDesconto The valorDesconto to set.
+     */
+    public void setValorDesconto(Double valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+    /**
+     * @param valorEntrada The valorEntrada to set.
+     */
+    public void setValorEntrada(Double valorEntrada) {
+        this.valorEntrada = valorEntrada;
+    }
+    /**
+     * @param valorParcela The valorParcela to set.
+     */
+    public void setValorParcela(Double valorParcela) {
+        this.valorParcela = valorParcela;
+    }
+    /**
+     * @param valorTotal The valorTotal to set.
+     */
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    /**
+     * @param valorTotalPrazo The valorTotalPrazo to set.
+     */
+    public void setValorTotalPrazo(Double valorTotalPrazo) {
+        this.valorTotalPrazo = valorTotalPrazo;
+    }
+    /**
+     * @param valorTotalProduto The valorTotalProduto to set.
+     */
+    public void setValorTotalProduto(Double valorTotalProduto) {
+        this.valorTotalProduto = valorTotalProduto;
+    }
+    
+    /**
+     * @hibernate.set lazy="true" inverse = "true" order-by="linha_numero" cascade = "none"
+     * @hibernate.collection-key column="cd_orcamento" 
+     * @hibernate.collection-one-to-many class = "org.appfuse.model.venda.OrcamentoItem"
+     */
+    public Set getOrcamentoItems() {
+	return this.orcamentoItems;
+    }
+
+    public void setOrcamentoItems(Set orcamentoItems) {
+        this.orcamentoItems = orcamentoItems;
+    }
+
+    /**
+     * Faz a totalização da nota fiscal.
+     * @return totalNota.
+     */
+    public Double getTotalInternal(){
+         double _totalDesconto = (getValorDesconto() == null) ? 0 : getValorDesconto().doubleValue();
+         double _totalProdutos = (getValorTotalProduto() == null) ? 0 : getValorTotalProduto().doubleValue();
+         
+         return new Double (_totalProdutos - _totalDesconto);
+    }
+    /**
+     * Totaliza o movimento de estoque. Realiza um loop sobre todos os itens
+     * do movimento e, para cada item chama item.getSubTotal(); e item.getValorDesconto();,
+     * para totalizar os campos totalProdutos e desconto na orcamento.
+     * @return Double 
+     */    
+    public Double getTotalProdutosInternal(){
+        double tempTotal = 0.0;
+        Iterator i = getOrcamentoItems().iterator();
+        setValorTotalProduto(new Double(0.0));
+        while (i.hasNext()) {
+            OrcamentoItem orcamentoItem = (OrcamentoItem) i.next();
+            tempTotal = tempTotal + orcamentoItem.getTotal().doubleValue();
+        }
+       return new Double(tempTotal); 
+    }    
+    
+    /**
+     * @see java.lang.Object#equals(Object)
+     */
+    public boolean equals(Object object) {
+        if (!(object instanceof Orcamento)) {
+            return false;
+        }
+        Orcamento rhs = (Orcamento) object;
+        return new EqualsBuilder().append(
+                this.valorTotalProduto, rhs.valorTotalProduto).append(
+                this.valorTotalPrazo, rhs.valorTotalPrazo).append(
+                this.valorEntrada, rhs.valorEntrada).append(this.valorParcela,
+                rhs.valorParcela).append(this.NomeCliente, rhs.NomeCliente)
+                .append(this.observacao, rhs.observacao).append(
+                        this.dtValidadeOrcamento, rhs.dtValidadeOrcamento)
+                .append(this.tipoPagamento, rhs.tipoPagamento).append(
+                        this.status, rhs.status).append(this.tipoCliente,
+                        rhs.tipoCliente).append(this.cdOrcamento,
+                        rhs.cdOrcamento).append(this.valorDesconto,
+                        rhs.valorDesconto).append(this.cdFuncionario,
+                        rhs.cdFuncionario)
+                .append(this.cdCliente, rhs.cdCliente).append(this.dtOrcamento,
+                        rhs.dtOrcamento)
+                .append(this.valorTotal, rhs.valorTotal).append(
+                        this.cdPlanoPagamento, rhs.cdPlanoPagamento).isEquals();
+    }
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return new ToStringBuilder(this).append("dtValidadeOrcamento",
+                this.dtValidadeOrcamento).append("valorTotalPrazo",
+                this.valorTotalPrazo).append("valorTotalProduto",
+                this.valorTotalProduto).append("valorParcela",
+                this.valorParcela).append("tipoCliente", this.tipoCliente)
+                .append("valorDesconto", this.valorDesconto).append(
+                        "observacao", this.observacao).append("status",
+                        this.status).append("nomeCliente",
+                        this.getNomeCliente()).append("valorTotal",
+                        this.valorTotal).append("cdCliente", this.cdCliente)
+                .append("dtOrcamento", this.dtOrcamento).append("cdOrcamento",
+                        this.cdOrcamento).append("cdPlanoPagamento",
+                        this.cdPlanoPagamento).append("cdFuncionario",
+                        this.cdFuncionario).append("valorEntrada",
+                        this.valorEntrada).append("tipoPagamento",
+                        this.tipoPagamento).toString();
+    }
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    public int hashCode() {
+        return new HashCodeBuilder(-1413895399, 1315420679).append(this.valorTotalProduto).append(
+                this.valorTotalPrazo).append(this.valorEntrada).append(
+                this.valorParcela).append(this.NomeCliente).append(
+                this.observacao).append(this.dtValidadeOrcamento).append(
+                this.tipoPagamento).append(this.status).append(
+                this.tipoCliente).append(this.cdOrcamento).append(
+                this.valorDesconto).append(this.cdFuncionario).append(
+                this.cdCliente).append(this.dtOrcamento)
+                .append(this.valorTotal).append(this.cdPlanoPagamento)
+                .toHashCode();
+    }
+}
---
+++ gfi/core/src/main/java/com/gilbertoca/gfi/venda/model/OrcamentoItem.java
@@ -0,0 +1,271 @@
+package com.gilbertoca.gfi.venda.model;
+
+import com.gilbertoca.gfi.inventario.model.Item;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+/**
+ * @hibernate.class table="construtor.venda_orcamento_item"
+ */  
+public class OrcamentoItem implements Serializable {
+    /** identifier field */
+    private OrcamentoItemPK comp_id = new OrcamentoItemPK();
+
+    /** 
+     * Usado para determinar se instancia é transient or detached 
+     */
+    private int version = -1;
+    private Double desconto = new Double(0);
+    private Float quantidade = new Float(0); 
+    private Double precoUnitario = new Double(0);
+    private Integer cdItem;
+    private Item item = new Item();
+    private Orcamento orcamento = new Orcamento();
+    /** 
+     * Usado para simular a chamada de método interno que realiza a totalização do item.
+     * Como não podemos chamar o método getTotal() na interface web, então simularemos 
+     * a chamada de um bean, ou seja, set e get para uma propriedade:
+     * 	- Criamos um atributo transiente: Total;
+     * 	- Criamos os metodos correspondentes set e get:
+     *  - No metodo getTotal(), chamados nossa rotinas de calculo, neste caso, getInternalTotal()  
+     */    
+    private Double total = new Double(0);
+    /** 
+     * Usado para simular a chamada de método interno que realiza a totalização do item.
+     * Como não podemos chamar o método getValorDesconto() na interface web, então simularemos 
+     * a chamada de um bean, ou seja, set e get para uma propriedade:
+     * 	- Criamos um atributo transiente: valorDesconto;
+     * 	- Criamos os metodos correspondentes set e get:
+     *  - No metodo getValorDesconto(), chamados nossa rotinas de calculo, neste caso, getInternalValorDesconto()  
+     */    
+    private Double valorDesconto = new Double(0);
+    
+    /** full constructor */
+    public OrcamentoItem(OrcamentoItemPK comp_id, Float quantidade,
+        Double precoPorQuantidade, Item item) {
+        this.comp_id = comp_id;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoPorQuantidade;
+        this.item = item;
+    } 
+
+    /** default constructor */
+    public OrcamentoItem() {
+    }
+
+    /**
+     *  @hibernate.id generator-class="assigned"
+     *
+     */
+    public OrcamentoItemPK getComp_id() {
+        return this.comp_id;
+    }
+
+    public void setComp_id(OrcamentoItemPK comp_id) {
+        this.comp_id = comp_id;
+    }
+
+    /**
+     *  @hibernate.property  column="quantidade"  not-null="true"
+     *
+     */
+    public Float getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(Float quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     *  @hibernate.property  column="desconto"
+     *	@return Returns the desconto.
+     */
+    public Double getDesconto() {
+        return desconto;
+    }
+    /**
+     * @param desconto The desconto to set.
+     */
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
+    /**
+     *            @hibernate.property
+     *             column="preco_unitario"
+     *             length="10"
+     *             not-null="true"
+     *
+     */
+    public Double getPrecoUnitario() {
+        return this.precoUnitario;
+    }
+
+    public void setPrecoUnitario(Double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    /**
+     * @return Returns the cdItem.
+     * @hibernate.property column="cd_item"
+     */
+    public Integer getCdItem() {
+        return cdItem;
+    }
+
+    /**
+     * @param cdItem The cdItem to set.
+     */
+    public void setCdItem(Integer cdItem) {
+        this.cdItem = cdItem;
+    }
+
+    /**
+     * @hibernate.many-to-one
+     *  insert="false" update="false" cascade="none" column="cd_item"
+     *  outer-join="true"
+     * @return a populated item object (based on the cdItem)
+     */
+    public Item getItem() {
+        return this.item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    /**
+     * @hibernate.many-to-one
+     *  insert="false" update="false" column="cd_orcamento"
+     *  outer-join="true"
+     * @return a populated orcamento object (based on the cdOrcamento)
+     */
+    public Orcamento getOrcamento() {
+        return this.orcamento;
+    }
+
+    public void setOrcamento(Orcamento orcamento) {
+        this.orcamento = orcamento;
+    }
+
+
+    public void addOrcamento(Orcamento orcamento) {
+           orcamento.getOrcamentoItems().add(this);
+           this.orcamento = orcamento;
+       }     
+
+    /**
+     * @return Returns the updated timestamp.
+     * @hibernate.version unsaved-value = "negative"
+     */
+    public int getVersion() {
+        return version;
+    }
+
+    /**
+     * @param updated The updated version to set.
+     */
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    /**
+     * Retorna o subtotal para este item, exemplo:
+     * Compra de dois itens no valor 22,00 e um desconto de 5%
+     * valor = precounitario * quantidade
+     * valordescontado = (valor/100)* desconto
+     * Total = valor - valorDescontado 
+     */
+    public Double getInternalTotal() {
+        double preco, quantidade, desconto; 
+        double valor = 0, valorDescontado = 0;
+        preco = (getPrecoUnitario() == null) ? 0 : getPrecoUnitario().doubleValue();
+        quantidade = (getQuantidade() == null) ? 0 : getQuantidade().doubleValue();
+        desconto = (getDesconto() == null) ? 0 : getDesconto().doubleValue();
+        
+    	valor = (preco * quantidade);
+    	valorDescontado = (valor / 100) * desconto;
+        return new Double(valor - valorDescontado);
+    }
+
+    /**
+     * @return Returns the total.
+     */
+    public Double getTotal() {
+        return getInternalTotal();
+    }
+    /**
+     * @param total The total to set.
+     */
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+    /**
+     * Retorna o valor do desconto para este item, exemplo:
+     * Compra de dois itens no valor 22,00 e um desconto de 5%
+     * valor = precounitario * quantidade
+     * valordescontado = (valor/100)* desconto 
+     */
+    public Double getInternalValorDesconto() {
+        double preco, quantidade, desconto; 
+        double valor = 0, valorDescontado = 0;
+        preco = (getPrecoUnitario() == null) ? 0 : getPrecoUnitario().doubleValue();
+        quantidade = (getQuantidade() == null) ? 0 : getQuantidade().doubleValue();
+        desconto = (getDesconto() == null) ? 0 : getDesconto().doubleValue();
+        
+    	valor = (preco * quantidade);
+    	valorDescontado = (valor / 100) * desconto;
+
+    	return new Double(valorDescontado);
+    }
+
+    /**
+     * @return Returns the valorDesconto.
+     */
+    public Double getValorDesconto() {
+        return getInternalValorDesconto();
+    }
+    /**
+     * @param valorDesconto The valorDesconto to set.
+     */
+    public void setValorDesconto(Double valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+    
+    /**
+     * @see java.lang.Object#equals(Object)
+     */
+    public boolean equals(Object object) {
+        if (!(object instanceof OrcamentoItem)) {
+            return false;
+        }
+        OrcamentoItem rhs = (OrcamentoItem) object;
+        return new EqualsBuilder().append(this.cdItem, rhs.cdItem).append(this.precoUnitario,
+                rhs.precoUnitario).append(this.quantidade, rhs.quantidade)
+                .append(this.desconto, rhs.desconto).append(
+                        this.comp_id, rhs.comp_id).append(this.version,
+                        rhs.version).isEquals();
+    }
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    public int hashCode() {
+        return new HashCodeBuilder(1478035765, 554094549).append(this.cdItem).append(this.precoUnitario).append(
+                        this.quantidade).append(this.desconto).append(this.comp_id).append(this.version).toHashCode();
+    }
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("desconto", this.desconto).append("version", this.version).append("quantidade",
+                        this.quantidade).append("comp_id", this.comp_id)
+                .append("valorDesconto", this.getInternalValorDesconto()).append(
+                        "total", this.getInternalTotal()).append("precoUnitario", this.precoUnitario).append("cdItem",
+                        this.cdItem).toString();
+    }
+}
