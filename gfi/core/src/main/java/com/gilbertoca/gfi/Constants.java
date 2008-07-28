920acf39fb51d36978cb04e59eb41fe8477bb7ee
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2008-07-28 20:56:04 +0000

    Ajustes
    git-svn-id: https://construtor.googlecode.com/svn/trunk@342 daf9df78-7b24-0410-bc62-633826f6319b

--- gfi/core/src/main/java/com/gilbertoca/gfi/Constants.java
+++ gfi/core/src/main/java/com/gilbertoca/gfi/Constants.java
@@ -1,45 +1,43 @@
 package com.gilbertoca.gfi;
+
 /**
  * Constant values used throughout the application.
- *
+ * 
  * <p>
  * <a href="Constants.java.html"><i>View Source</i></a>
  * </p>
- *
+ * 
  * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
  */
 public class Constants {
-    //~ Static fields/initializers =============================================
-   /**
-    * O nome do arquivo de propriedades para construção do DBCPDataSource
-    */    
-  public static final String DBCPCONFIGFILE = "/dbcp.properties";
-   /**
-    * O nome do DataSource local
-    */    
-  public static final String DBCPDATASOURCE = "dbcp";
-   /**
-    * O nome do DataSource remoto
-    */    
-  public static final String DATASOURCE = "gfi";
-   /**
-    * O nome do Módulo gerenciado pelo ORM OrBroker. Podemos dividi-lo em mais modulos, como:
-    * orbroker-financeiro.xml,
-    * orbroker-inventario.xml
-    */    
-  public static final String ORBROKER_INVENTARIO = "/META-INF/orbroker-inventario.xml";
-  
-    
-   /**
-    * O identificador da UF utilizado pelo logradouro
-    * UF=Estado do Tocantins.
-    */    
-  public static final String FAIXAUF_UFESG = "TO";
-   /**
-    * O identificador da tipoLogradouro utilizado pelo logradouro
-    * Rua=Rua.
-    */    
-  public static final String TIPOLOGR_TIPOLOGRADOURO = "Rua";	  
+    // ~ Static fields/initializers
+    // =============================================
+    /**
+     * O nome do arquivo de propriedades para construção do DBCPDataSource
+     */
+    public static final String DBCPCONFIGFILE = "/dbcp.properties";
+    /**
+     * O nome do DataSource local
+     */
+    public static final String DBCPDATASOURCE = "dbcp";
+    /**
+     * O nome do DataSource remoto
+     */
+    public static final String DATASOURCE = "gfi";
+    /**
+     * O nome do Módulo gerenciado pelo ORM OrBroker. Podemos dividi-lo em mais
+     * modulos, como: orbroker-financeiro.xml, orbroker-inventario.xml
+     */
+    public static final String ORBROKER_INVENTARIO = "/META-INF/orbroker-inventario.xml";
+
+    /**
+     * O identificador da UF utilizado pelo logradouro UF=Estado do Tocantins.
+     */
+    public static final String FAIXAUF_UFESG = "TO";
+    /**
+     * O identificador da tipoLogradouro utilizado pelo logradouro Rua=Rua.
+     */
+    public static final String TIPOLOGR_TIPOLOGRADOURO = "Rua";
 
     /**
      * O identificador da natureza da operação utilizado pela venda.
@@ -47,31 +45,31 @@ public class Constants {
      */
     public static final String NATUREZAOPERACAO_VENDA = "5.101";
     /**
-     * O identificador da conta caixa utilizado pela venda, em uma venda a vista.
-     * 4=Vendas.
-     */    
-   public static final String CAIXACONTA_VENDA = "4";
+     * O identificador da conta caixa utilizado pela venda, em uma venda a
+     * vista. 4=Vendas.
+     */
+    public static final String CAIXACONTA_VENDA = "4";
     /**
-     * O identificador da conta caixa utilizado pela venda, em uma venda a prazo com cheque-pre.
-     * 85=Vendas com Cheque-Pré.
-     */    
-   public static final String CAIXACONTA_VENDA_CHEQUE = "85";   
+     * O identificador da conta caixa utilizado pela venda, em uma venda a prazo
+     * com cheque-pre. 85=Vendas com Cheque-Pré.
+     */
+    public static final String CAIXACONTA_VENDA_CHEQUE = "85";
 
-   /**
-    * O identificador da conta caixa utilizado pela Conta a Receber, em uma venda a prazo
-    * 5=Recebimentos de Contas.
-    */    
-  public static final String CAIXACONTA_CONTA_RECEBER = "5";
+    /**
+     * O identificador da conta caixa utilizado pela Conta a Receber, em uma
+     * venda a prazo 5=Recebimentos de Contas.
+     */
+    public static final String CAIXACONTA_CONTA_RECEBER = "5";
 
-  /**
-   * O identificador da conta caixa utilizado pela Conta a Pagar, em uma compra a prazo
-   * 5=Pagamentos de Contas.
-   */    
- public static final String CAIXACONTA_CONTA_PAGAR = "68";
-  
-  /**
-   * O identificador da cidade utilizado no componente Endereço.
-   * 9902=Paraíso do Tocantins.
-   */    
- public static final String LOCALIDADE = "9902";  
+    /**
+     * O identificador da conta caixa utilizado pela Conta a Pagar, em uma
+     * compra a prazo 5=Pagamentos de Contas.
+     */
+    public static final String CAIXACONTA_CONTA_PAGAR = "68";
+
+    /**
+     * O identificador da cidade utilizado no componente Endereço. 9902=Paraíso
+     * do Tocantins.
+     */
+    public static final String LOCALIDADE = "9902";
 }
--- gfi/core/src/main/java/com/gilbertoca/gfi/service/IService.java
+++ gfi/core/src/main/java/com/gilbertoca/gfi/service/IService.java
@@ -9,10 +9,9 @@ import java.util.Collection;
 
 import net.sourceforge.orbroker.Broker;
 
-
 /**
- * Interface geral para uso com o mescanimo de persistência OrBroker. Estabelece assinaturas
- * de métodos essenciais para operações de acesso a banco de dados.
+ * Interface geral para uso com o mescanimo de persistência OrBroker. Estabelece
+ * assinaturas de métodos essenciais para operações de acesso a banco de dados.
  * 
  * @author Gilberto Caetano de Andrade
  */
@@ -21,44 +20,67 @@ public interface IService<T, PK extends Serializable> {
      * Expõe o mecanismo de persistência. .
      */
     public Broker getBroker();
+
     public Class<T> getClassEntity();
+
     /**
-     * Método genérico usado para obter todas as entidades de uma classe particular. Essa classe
-     * particular é definida no construtor da implementação desta interface. 
-     * @return Collection de entidades preenchidas
-     */    
-    public Collection<T> getAll();
-    /**
-     * Método genérico para obter uma entidade. 
+     * Método genérico usado para obter todas as entidades de uma classe
+     * particular. Essa classe particular é definida no construtor da
+     * implementação desta interface.
      * 
-     * @param entity objeto ao qual se aplica o retorno da pesquisa.
-     * @return true e o preenchimento da entidade passada como parâmetro. false caso contrário. 
+     * @return Collection de entidades preenchidas
+     */
+    public Collection<T> getAll();
+
+    /**
+     * Método genérico para obter uma entidade.
+     * 
+     * @param entity
+     *                objeto ao qual se aplica o retorno da pesquisa.
+     * @return true e o preenchimento da entidade passada como parâmetro. false
+     *         caso contrário.
      */
     public boolean find(T entity);
+
     /**
      * Método genérico para inserção de uma nova entidade.
-     * @param entity entidade a ser inserida.
+     * 
+     * @param entity
+     *                entidade a ser inserida.
      */
     public void insert(T entity);
+
     /**
      * Método genérico para atualização de uma entidade.
-     * @param entity entidade a ser alterada.
-     */    
+     * 
+     * @param entity
+     *                entidade a ser alterada.
+     */
     public void update(T entity);
+
     /**
      * Método genérico para deleção de uma entidade.
-     * @param entity entidade a ser deletada.
-     */    
+     * 
+     * @param entity
+     *                entidade a ser deletada.
+     */
     public void delete(T entity);
+
     /**
      * Método genérico para consulta nomeada preexistente.
-     * @param queryName nome da consulta.
-     */    
+     * 
+     * @param queryName
+     *                nome da consulta.
+     */
     public Collection<T> findByNamedQuery(String queryName);
+
     /**
      * Método genérico para personalizada com a cláusula SQL LIKE.
-     * @param likeColumn campo a ser usado na cláusula SQL.
-     * @param likeValue valor para pesquisa. 
-     */    
+     * 
+     * @param likeColumn
+     *                campo a ser usado na cláusula SQL.
+     * @param likeValue
+     *                valor para pesquisa.
+     */
     public Collection<T> findLike(String likeColumn, String likeValue);
 }
