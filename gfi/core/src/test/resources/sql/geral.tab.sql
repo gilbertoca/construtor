35f98340b2138528659f6de426d2636bb83c39c7
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2008-07-28 20:52:33 +0000

    Módulo geral.
    git-svn-id: https://construtor.googlecode.com/svn/trunk@341 daf9df78-7b24-0410-bc62-633826f6319b

---
+++ gfi/core/src/test/resources/sql/geral.tab.sql
@@ -0,0 +1,16 @@
+drop table gfi.inventario_unidade_medida;
+drop table gfi.inventario_categoria;
+create table gfi.inventario_unidade_medida (
+   cd_unidade_medida varchar(4) not null,
+   version integer not null,
+   descricao_unidade varchar(30),
+   primary key (cd_unidade_medida)
+);
+create table gfi.inventario_categoria (
+   cd_categoria integer not null,
+   nome_categoria varchar(80),
+   descricao_categoria varchar(255),
+   dt_cadastro timestamp not null,
+   version integer not null,
+   primary key (cd_categoria)
+);
