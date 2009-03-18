900f2625bc384861694206d30cd351cb913f5736
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2009-03-18 19:29:14 +0000

    indentação
    git-svn-id: https://construtor.googlecode.com/svn/trunk@372 daf9df78-7b24-0410-bc62-633826f6319b

--- gfi/core/src/main/java/com/gilbertoca/gfi/service/BaseService.java
+++ gfi/core/src/main/java/com/gilbertoca/gfi/service/BaseService.java
@@ -43,199 +43,199 @@ import com.gilbertoca.gfi.Constants;
  * @author Gilberto Caetano de Andrade
  */
 public class BaseService<T, PK extends Serializable> implements IService<T, PK>{
-    protected String brokerName;
-    private Class<T> classEntity;
-    protected transient final Logger log = LoggerFactory.getLogger(getClass());
+	protected String brokerName;
+	private Class<T> classEntity;
+	protected transient final Logger log = LoggerFactory.getLogger(getClass());
 
-    public BaseService(Class<T> classEntity) {
-	super();
-	this.classEntity = classEntity;
-    }
-    public BaseService(Class<T> classEntity, String brokerName) {
-	super();
-	this.classEntity = classEntity;
-	this.brokerName = brokerName;
-    }
-
-    public Class<T> getClassEntity() {return classEntity;}
-
-    protected void setBrokerName(String brokerName) {
-	this.brokerName = brokerName;
-    }
-    public Collection<T> getAll(){
-	log.debug("Realizando consulta retornando todos os registros, para a entidade: {} ",getClassEntity());
-
-	if (getClassEntity() == null) {
-	    throw new IllegalArgumentException("ClassEntity não pode ser nulo!");
+	public BaseService(Class<T> classEntity) {
+		super();
+		this.classEntity = classEntity;
 	}
-	Query qry = getBroker().startQuery();        
-	String sentenca = "get" + getClassEntity().getSimpleName();
-
-	try {
-	    log.debug("Verificando parametros ..." +
-		    "\nNome senteça SQL: {}", sentenca);
-
-	    return findByNamedQuery(sentenca);
-	} finally {
-	    qry.close();
-	}    	
-    }
-
-
-    public boolean find(T entity) {
-	log.debug("Realizando consulta por entidade. Identificador usado como parâmetro: {} ",entity);
-
-	if (entity == null) {
-	    throw new IllegalArgumentException("Entidade não pode ser nulo!");
+	public BaseService(Class<T> classEntity, String brokerName) {
+		super();
+		this.classEntity = classEntity;
+		this.brokerName = brokerName;
 	}
-	Query qry = getBroker().startQuery();        
-	String parametro = entity.getClass().getSimpleName().substring(0, 1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
-	String sentenca = "get" + entity.getClass().getSimpleName();
-	qry.setParameter(parametro, entity);
-	try {
-	    log.debug("Verificando parametros ..." +
-		    "\nQuery parameter: {}" +
-		    "\nStament name   : {}" +
-		    "\nArgumento: {}\n", new Object[] {parametro, sentenca, entity});
-	    return qry.selectOne(sentenca, entity);
-	} finally {
-	    qry.close();
+
+	public Class<T> getClassEntity() {return classEntity;}
+
+	protected void setBrokerName(String brokerName) {
+		this.brokerName = brokerName;
 	}
-    }
+	public Collection<T> getAll(){
+		log.debug("Realizando consulta retornando todos os registros, para a entidade: {} ",getClassEntity());
 
-    public void insert(T entity) throws InsertException{
-	log.debug("Realizando inserção. Entidade usada como parâmetro: {} ",entity);    	
+		if (getClassEntity() == null) {
+			throw new IllegalArgumentException("ClassEntity não pode ser nulo!");
+		}
+		Query qry = getBroker().startQuery();        
+		String sentenca = "get" + getClassEntity().getSimpleName();
 
-	if (entity == null) {
-	    throw new IllegalArgumentException("Entidade não pode ser null!");
+		try {
+			log.debug("Verificando parametros ..." +
+					"\nNome senteça SQL: {}", sentenca);
+
+			return findByNamedQuery(sentenca);
+		} finally {
+			qry.close();
+		}    	
 	}
-	Transaction txn = getBroker().startTransaction();        
-	String parametro = entity.getClass().getSimpleName().substring(0, 1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
-	String sentenca = "insert" + entity.getClass().getSimpleName();
-	int recordsUpdated = 0;
-	try {
-	    log.debug("Verificando parametros ..." +
-		    "\nParâmetro: {}" +
-		    "\nNome senteça SQL: {}", parametro, sentenca);
 
-	    txn.setParameter(parametro, entity);
-	    recordsUpdated = txn.execute(sentenca);
-	    if (recordsUpdated != 1) {
-		txn.rollback();
-		log.debug("Problemas na inserção. Nº registros afetados: {} ",recordsUpdated);
-		throw new InsertException("Incapaz de realizar inserção!");
-	    }
-	    txn.commit();
-	    log.debug("Inserção realizada com sucesso.");
-	} finally {
-	    txn.close();
+
+	public boolean find(T entity) {
+		log.debug("Realizando consulta por entidade. Identificador usado como parâmetro: {} ",entity);
+
+		if (entity == null) {
+			throw new IllegalArgumentException("Entidade não pode ser nulo!");
+		}
+		Query qry = getBroker().startQuery();        
+		String parametro = entity.getClass().getSimpleName().substring(0, 1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
+		String sentenca = "get" + entity.getClass().getSimpleName();
+		qry.setParameter(parametro, entity);
+		try {
+			log.debug("Verificando parametros ..." +
+					"\nQuery parameter: {}" +
+					"\nStament name   : {}" +
+					"\nArgumento: {}\n", new Object[] {parametro, sentenca, entity});
+			return qry.selectOne(sentenca, entity);
+		} finally {
+			qry.close();
+		}
 	}
-    }
 
-    public void update(T entity){
-	log.debug("Realizando alteração. Entidade usada como parâmetro: {} ",entity);
+	public void insert(T entity) throws InsertException{
+		log.debug("Realizando inserção. Entidade usada como parâmetro: {} ",entity);    	
 
-	if (entity == null) {
-	    throw new IllegalArgumentException("Entidade não pode ser nulo!");
+		if (entity == null) {
+			throw new IllegalArgumentException("Entidade não pode ser null!");
+		}
+		Transaction txn = getBroker().startTransaction();        
+		String parametro = entity.getClass().getSimpleName().substring(0, 1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
+		String sentenca = "insert" + entity.getClass().getSimpleName();
+		int recordsUpdated = 0;
+		try {
+			log.debug("Verificando parametros ..." +
+					"\nParâmetro: {}" +
+					"\nNome senteça SQL: {}", parametro, sentenca);
+
+			txn.setParameter(parametro, entity);
+			recordsUpdated = txn.execute(sentenca);
+			if (recordsUpdated != 1) {
+				txn.rollback();
+				log.debug("Problemas na inserção. Nº registros afetados: {} ",recordsUpdated);
+				throw new InsertException("Incapaz de realizar inserção!");
+			}
+			txn.commit();
+			log.debug("Inserção realizada com sucesso.");
+		} finally {
+			txn.close();
+		}
 	}
-	Transaction txn = getBroker().startTransaction();        
-	String parametro = entity.getClass().getSimpleName().substring(0, 1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
-	String sentenca = "update" + entity.getClass().getSimpleName();
 
-	int recordsUpdated = 0;
-	try {
-	    log.debug("Verificando parametros ..." +
-		    "\nParâmetro: {}" +
-		    "\nNome senteça SQL: {}", parametro, sentenca);
+	public void update(T entity){
+		log.debug("Realizando alteração. Entidade usada como parâmetro: {} ",entity);
 
-	    txn.setParameter(parametro, entity);
-	    recordsUpdated = txn.execute(sentenca);
-	    if (recordsUpdated != 1) {
-		txn.rollback();
-		log.debug("Problemas na alteração. Nº registros afetados: {} ",recordsUpdated);
-		//throw new ThatsWeirdException();
-	    }
-	    txn.commit();
-	    log.debug("Alteração realizada com sucesso.");
-	} finally {
-	    txn.close();
+		if (entity == null) {
+			throw new IllegalArgumentException("Entidade não pode ser nulo!");
+		}
+		Transaction txn = getBroker().startTransaction();        
+		String parametro = entity.getClass().getSimpleName().substring(0, 1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
+		String sentenca = "update" + entity.getClass().getSimpleName();
+
+		int recordsUpdated = 0;
+		try {
+			log.debug("Verificando parametros ..." +
+					"\nParâmetro: {}" +
+					"\nNome senteça SQL: {}", parametro, sentenca);
+
+			txn.setParameter(parametro, entity);
+			recordsUpdated = txn.execute(sentenca);
+			if (recordsUpdated != 1) {
+				txn.rollback();
+				log.debug("Problemas na alteração. Nº registros afetados: {} ",recordsUpdated);
+				//throw new ThatsWeirdException();
+			}
+			txn.commit();
+			log.debug("Alteração realizada com sucesso.");
+		} finally {
+			txn.close();
+		}
 	}
-    }
 
-    public void delete(T entity){
-	log.debug("Realizando deleção. Entidade usada como parâmetro: {} ",entity);
+	public void delete(T entity){
+		log.debug("Realizando deleção. Entidade usada como parâmetro: {} ",entity);
 
-	if (entity == null) {
-	    throw new IllegalArgumentException("Entidade não pode ser nulo!");
+		if (entity == null) {
+			throw new IllegalArgumentException("Entidade não pode ser nulo!");
+		}
+		Transaction txn = getBroker().startTransaction();        
+		String parametro = entity.getClass().getSimpleName().substring(0, 1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
+		String sentenca = "delete" + entity.getClass().getSimpleName();
+
+		int recordsUpdated = 0;
+		try {
+			log.debug("Verificando parametros ..." +
+					"\nParâmetro: {}" +
+					"\nNome senteça SQL: {}", parametro, sentenca);
+
+			txn.setParameter(parametro, entity);
+			recordsUpdated = txn.execute(sentenca);
+			if (recordsUpdated != 1) {
+				txn.rollback();
+				log.debug("Problemas na deleção. Nº registros afetados: {} ",recordsUpdated);                
+				//throw new ThatsWeirdException();
+			}else{
+				txn.commit();
+				log.debug("Deleção realizada com sucesso.");
+			}
+		} finally {
+			txn.close();
+		}
 	}
-	Transaction txn = getBroker().startTransaction();        
-	String parametro = entity.getClass().getSimpleName().substring(0, 1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
-	String sentenca = "delete" + entity.getClass().getSimpleName();
 
-	int recordsUpdated = 0;
-	try {
-	    log.debug("Verificando parametros ..." +
-		    "\nParâmetro: {}" +
-		    "\nNome senteça SQL: {}", parametro, sentenca);
+	public Collection<T> findLike(String likeColumn, String likeValue) {
+		log.debug("Realizando consulta retornando registros, para entidade {} estejam no filtro: columa {} e valor {} ", new Object[] {getClassEntity(), likeColumn, likeValue});
 
-	    txn.setParameter(parametro, entity);
-	    recordsUpdated = txn.execute(sentenca);
-	    if (recordsUpdated != 1) {
-		txn.rollback();
-		log.debug("Problemas na deleção. Nº registros afetados: {} ",recordsUpdated);                
-		//throw new ThatsWeirdException();
-	    }else{
-		txn.commit();
-		log.debug("Deleção realizada com sucesso.");
-	    }
-	} finally {
-	    txn.close();
+		if (getClassEntity() == null) {
+			throw new IllegalArgumentException("ClassEntity não pode ser nulo!");
+		}
+		if (likeColumn == null || likeColumn.equals("")) {
+			throw new IllegalArgumentException(
+			"likeColumn não pode ser nulo!");
+		}
+		if (likeValue == null || likeValue.equals("")) {
+			throw new IllegalArgumentException(
+			"likeValue não pode ser nulo!");
+		}
+		Query qry = getBroker().startQuery();    	
+		String parametro = "likeValue";
+		String sentenca = "get" + getClassEntity().getSimpleName();
+		qry.setTextReplacement("likeColumn", likeColumn);
+		qry.setParameter(parametro, likeValue);        
+		try {
+			log.debug("Verificando parametros ..." +
+					"\nTextReplacement   : {}" +
+					"\nQuery parameter: {}" +
+					"\nStament name   : {}" +
+					"\nArgumento: {}\n", new Object[] {likeColumn, parametro, sentenca, likeValue});
+			return qry.selectMany(sentenca);
+		} finally {
+			qry.close();
+		}    	
 	}
-    }
 
-    public Collection<T> findLike(String likeColumn, String likeValue) {
-	log.debug("Realizando consulta retornando registros, para entidade {} estejam no filtro: columa {} e valor {} ", new Object[] {getClassEntity(), likeColumn, likeValue});
-
-	if (getClassEntity() == null) {
-	    throw new IllegalArgumentException("ClassEntity não pode ser nulo!");
+	public Collection<T> findByNamedQuery(String queryName){
+		Query qry = getBroker().startQuery();
+		try {
+			return qry.selectMany(queryName);
+		} finally {
+			qry.close();
+		}
 	}
-	if (likeColumn == null || likeColumn.equals("")) {
-	    throw new IllegalArgumentException(
-	    "likeColumn não pode ser nulo!");
+	public Broker getBroker() {
+		return ResourceLocator.getInstance().getBroker(brokerName, Constants.ORBROKER_SCHEMA);
 	}
-	if (likeValue == null || likeValue.equals("")) {
-	    throw new IllegalArgumentException(
-	    "likeValue não pode ser nulo!");
-	}
-	Query qry = getBroker().startQuery();    	
-	String parametro = "likeValue";
-	String sentenca = "get" + getClassEntity().getSimpleName();
-	qry.setTextReplacement("likeColumn", likeColumn);
-	qry.setParameter(parametro, likeValue);        
-	try {
-	    log.debug("Verificando parametros ..." +
-		    "\nTextReplacement   : {}" +
-		    "\nQuery parameter: {}" +
-		    "\nStament name   : {}" +
-		    "\nArgumento: {}\n", new Object[] {likeColumn, parametro, sentenca, likeValue});
-	    return qry.selectMany(sentenca);
-	} finally {
-	    qry.close();
-	}    	
-    }
-
-    public Collection<T> findByNamedQuery(String queryName){
-	Query qry = getBroker().startQuery();
-	try {
-	    return qry.selectMany(queryName);
-	} finally {
-	    qry.close();
-	}
-    }
-    public Broker getBroker() {
-	return ResourceLocator.getInstance().getBroker(brokerName, Constants.ORBROKER_SCHEMA);
-    }
-    public Broker getBroker(String brokerName) {
-	return ResourceLocator.getInstance().getBroker(brokerName, Constants.ORBROKER_SCHEMA);
-    }    
+	public Broker getBroker(String brokerName) {
+		return ResourceLocator.getInstance().getBroker(brokerName, Constants.ORBROKER_SCHEMA);
+	}    
 }
