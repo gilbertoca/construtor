f8c9d7a023f816ec01bbd553b3c21aacaceb6d81
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2009-12-23 20:51:27 +0000

    Relacionamento um para um
    git-svn-id: https://construtor.googlecode.com/svn/trunk@380 daf9df78-7b24-0410-bc62-633826f6319b

--- park-jpa/core/src/main/java/park/model/Customer.java
+++ park-jpa/core/src/main/java/park/model/Customer.java
@@ -11,77 +11,88 @@ import java.util.Set;
  */
 @Entity
 @Table(name = "CUSTOMER")
-@NamedQueries( { @NamedQuery(name = Customer.FIND_ALL, query = "SELECT c FROM Customer c") })
+@NamedQueries({
+    @NamedQuery(name = Customer.FIND_ALL, query = "SELECT c FROM Customer c")})
 public class Customer implements Serializable {
-	private static final long serialVersionUID = 1L;
-	public static final String FIND_ALL = "findAllCustomer";
-	@Id
-	@Column(name = "CD_CUSTOMER")
-	private Integer cdCustomer;
-	@OneToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "CD_CUSTOMER", insertable = false, updatable = false)
-	private Person person;
 
-	@Column(name = "PAYMENT_DAY")
-	private Integer paymentDay;
+    private static final long serialVersionUID = 1L;
+    public static final String FIND_ALL = "findAllCustomer";
+    @Id
+    @Column(name = "CD_CUSTOMER" )
+    @GeneratedValue
+    private Integer cdCustomer;
 
-	// bi-directional many-to-one association to Vehicle
-	@OneToMany(mappedBy = "customer")
-	private Set<Vehicle> vehicles;
+    @OneToOne
+    @JoinColumn(name = "CD_PERSON")
+    private Person person;
+    
+    @Column(name = "PAYMENT_DAY")
+    private Integer paymentDay;
+    // bi-directional many-to-one association to Vehicle
+    @OneToMany(mappedBy = "customer")
+    private Set<Vehicle> vehicles;
 
-	public Customer() {
-	}
+    public Customer() {
+    }
 
-	/**
-	 * @param cdCustomer
-	 * @param paymentDay
-	 */
-	public Customer(Integer cdCustomer, Integer paymentDay) {
-		this.cdCustomer = cdCustomer;
-		this.paymentDay = paymentDay;
-	}
+    /**
+     * @param cdCustomer
+     * @param paymentDay
+     */
+    public Customer( Integer CdCustomer, Integer paymentDay) {
+        this.cdCustomer = CdCustomer;
+        this.paymentDay = paymentDay;
+    }
 
-	public Integer getCdCustomer() {
-		return this.cdCustomer;
-	}
+    public Integer getCdPerson() {
+        return this.cdCustomer;
+    }
 
-	public void setCdCustomer(Integer cdCustomer) {
-		this.cdCustomer = cdCustomer;
-	}
+    /**
+     * @param person
+     *            the person to set
+     */
+    public void setPerson(Person person) {
+        this.person = person;
 
-	/**
-	 * @param person
-	 *            the person to set
-	 */
-	public void setPerson(Person person) {
-		this.person = person;
-	}
 
-	/**
-	 * @return the person
-	 */
-	public Person getPerson() {
-		return person;
-	}
+    }
 
-	public Integer getPaymentDay() {
-		return this.paymentDay;
-	}
+    /**
+     * @return the person
+     */
+    public Person getPerson() {
+        return person;
 
-	public void setPaymentDay(Integer paymentDay) {
-		this.paymentDay = paymentDay;
-	}
 
-	public Set<Vehicle> getVehicles() {
-		return this.vehicles;
-	}
+    }
 
-	public void setVehicles(Set<Vehicle> vehicles) {
-		this.vehicles = vehicles;
-	}
+    public Integer getPaymentDay() {
+        return this.paymentDay;
 
-	public String getName() {
-		return getPerson().getName();
-	}
 
+    }
+
+    public void setPaymentDay(Integer paymentDay) {
+        this.paymentDay = paymentDay;
+
+
+    }
+
+    public Set<Vehicle> getVehicles() {
+        return this.vehicles;
+
+
+    }
+
+    public void setVehicles(Set<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+
+
+    }
+
+    public String getName() {
+        return getPerson().getName();
+
+    }
 }
