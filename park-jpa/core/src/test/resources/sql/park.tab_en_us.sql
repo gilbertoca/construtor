656e7b051b2dad0dcbda19f832acfdb7117b5822
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2010-01-08 21:03:30 +0000

    uma alternativa
    git-svn-id: https://construtor.googlecode.com/svn/trunk@411 daf9df78-7b24-0410-bc62-633826f6319b

--- park-jpa/core/src/test/resources/sql/park.tab_en_us.sql
+++ park-jpa/core/src/test/resources/sql/park.tab_en_us.sql
@@ -9,7 +9,7 @@ DROP TABLE PERSON CASCADE;
 DROP TABLE PRICE_TABLE CASCADE;
 DROP TABLE VEHICLE_TYPE CASCADE;
 
-CREATE TABLE park.vehicle_type (
+CREATE TABLE vehicle_type (
                 v_type VARCHAR(20) NOT NULL,
                 manufacturer VARCHAR(50),
                 model VARCHAR(20),
@@ -17,7 +17,7 @@ CREATE TABLE park.vehicle_type (
 );
 
 
-CREATE TABLE park.price_table (
+CREATE TABLE price_table (
                 cd_price_table INTEGER NOT NULL,
                 item VARCHAR(50) NOT NULL,
                 price NUMERIC(1000,0) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE park.price_table (
 );
 
 
-CREATE TABLE park.person (
+CREATE TABLE person (
                 cd_person INTEGER NOT NULL,
                 address VARCHAR(100),
                 name VARCHAR(100) NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE park.person (
 );
 
 
-CREATE TABLE park.natural_person (
+CREATE TABLE natural_person (
                 cd_person INTEGER NOT NULL,
                 dt_birth DATE,
                 legal_document VARCHAR(20),
@@ -42,7 +42,7 @@ CREATE TABLE park.natural_person (
 );
 
 
-CREATE TABLE park.legal_entity (
+CREATE TABLE legal_entity (
                 cd_person INTEGER NOT NULL,
                 taxpayers_id VARCHAR(20) NOT NULL,
                 dt_foundation DATE NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE park.legal_entity (
 );
 
 
-CREATE TABLE park.parking (
+CREATE TABLE parking (
                 cd_parking INTEGER NOT NULL,
                 cd_person INTEGER NOT NULL,
                 parking_spaces INTEGER,
@@ -59,10 +59,10 @@ CREATE TABLE park.parking (
 
 
 CREATE UNIQUE INDEX parking_idx
- ON park.parking
+ ON parking
  ( cd_parking, cd_person );
 
-CREATE TABLE park.employee (
+CREATE TABLE employee (
                 cd_employee INTEGER NOT NULL,
                 cd_person INTEGER NOT NULL,
                 dt_admission DATE NOT NULL,
@@ -74,10 +74,10 @@ CREATE TABLE park.employee (
 
 
 CREATE UNIQUE INDEX employee_idx
- ON park.employee
+ ON employee
  ( cd_person );
 
-CREATE TABLE park.customer (
+CREATE TABLE customer (
                 cd_customer INTEGER NOT NULL,
                 cd_person INTEGER NOT NULL,
                 payment_day INTEGER,
@@ -86,10 +86,10 @@ CREATE TABLE park.customer (
 
 
 CREATE UNIQUE INDEX customer_idx
- ON park.customer
+ ON customer
  ( cd_person );
 
-CREATE TABLE park.vehicle (
+CREATE TABLE vehicle (
                 license_plate VARCHAR(20) NOT NULL,
                 v_type VARCHAR(20) NOT NULL,
                 cd_customer INTEGER NOT NULL,
@@ -99,7 +99,7 @@ CREATE TABLE park.vehicle (
 );
 
 
-CREATE TABLE park.stay (
+CREATE TABLE stay (
                 cd_stay INTEGER NOT NULL,
                 cd_parking INTEGER NOT NULL,
                 dt_entrance DATE NOT NULL,
@@ -116,96 +116,96 @@ CREATE TABLE park.stay (
 
 
 CREATE UNIQUE INDEX stay_idx
- ON park.stay
+ ON stay
  ( cd_stay, cd_parking, dt_entrance, hr_entrance, cd_employee_entrance, license_plate );
 
-ALTER TABLE park.vehicle ADD CONSTRAINT vehicle_v_type_fkey
+ALTER TABLE vehicle ADD CONSTRAINT vehicle_v_type_fkey
 FOREIGN KEY (v_type)
-REFERENCES park.vehicle_type (v_type)
+REFERENCES vehicle_type (v_type)
 ON DELETE RESTRICT
 ON UPDATE RESTRICT
 NOT DEFERRABLE;
 
-ALTER TABLE park.vehicle ADD CONSTRAINT vehicle_cd_price_table_fkey
+ALTER TABLE vehicle ADD CONSTRAINT vehicle_cd_price_table_fkey
 FOREIGN KEY (cd_price_table)
-REFERENCES park.price_table (cd_price_table)
+REFERENCES price_table (cd_price_table)
 ON DELETE RESTRICT
 ON UPDATE RESTRICT
 NOT DEFERRABLE;
 
-ALTER TABLE park.customer ADD CONSTRAINT customer_cd_person_fk
+ALTER TABLE customer ADD CONSTRAINT customer_cd_person_fk
 FOREIGN KEY (cd_person)
-REFERENCES park.person (cd_person)
+REFERENCES person (cd_person)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE park.legal_entity ADD CONSTRAINT legalentity_cd_person_fkey
+ALTER TABLE legal_entity ADD CONSTRAINT legalentity_cd_person_fkey
 FOREIGN KEY (cd_person)
-REFERENCES park.person (cd_person)
+REFERENCES person (cd_person)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE park.natural_person ADD CONSTRAINT naturalperson_cd_person_fkey
+ALTER TABLE natural_person ADD CONSTRAINT naturalperson_cd_person_fkey
 FOREIGN KEY (cd_person)
-REFERENCES park.person (cd_person)
+REFERENCES person (cd_person)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE park.employee ADD CONSTRAINT employee_cd_person_fk
+ALTER TABLE employee ADD CONSTRAINT employee_cd_person_fk
 FOREIGN KEY (cd_person)
-REFERENCES park.natural_person (cd_person)
+REFERENCES natural_person (cd_person)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE park.parking ADD CONSTRAINT parking_cd_person_fk
+ALTER TABLE parking ADD CONSTRAINT parking_cd_person_fk
 FOREIGN KEY (cd_person)
-REFERENCES park.legal_entity (cd_person)
+REFERENCES legal_entity (cd_person)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE park.employee ADD CONSTRAINT parking_cd_employee_fkey
+ALTER TABLE employee ADD CONSTRAINT parking_cd_employee_fkey
 FOREIGN KEY (cd_parking)
-REFERENCES park.parking (cd_parking)
+REFERENCES parking (cd_parking)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE park.stay ADD CONSTRAINT stay_cd_parking_fkey
+ALTER TABLE stay ADD CONSTRAINT stay_cd_parking_fkey
 FOREIGN KEY (cd_parking)
-REFERENCES park.parking (cd_parking)
+REFERENCES parking (cd_parking)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE park.stay ADD CONSTRAINT stay_cd_employee_outgoing_fkey
+ALTER TABLE stay ADD CONSTRAINT stay_cd_employee_outgoing_fkey
 FOREIGN KEY (cd_employee_outgoing)
-REFERENCES park.employee (cd_employee)
+REFERENCES employee (cd_employee)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE park.stay ADD CONSTRAINT stay_cd_employee_entrance_fkey
+ALTER TABLE stay ADD CONSTRAINT stay_cd_employee_entrance_fkey
 FOREIGN KEY (cd_employee_entrance)
-REFERENCES park.employee (cd_employee)
+REFERENCES employee (cd_employee)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE park.vehicle ADD CONSTRAINT vehicle_cd_customer_fkey
+ALTER TABLE vehicle ADD CONSTRAINT vehicle_cd_customer_fkey
 FOREIGN KEY (cd_customer)
-REFERENCES park.customer (cd_customer)
+REFERENCES customer (cd_customer)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE park.stay ADD CONSTRAINT stay_license_plate_fkey
+ALTER TABLE stay ADD CONSTRAINT stay_license_plate_fkey
 FOREIGN KEY (license_plate)
-REFERENCES park.vehicle (license_plate)
+REFERENCES vehicle (license_plate)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
