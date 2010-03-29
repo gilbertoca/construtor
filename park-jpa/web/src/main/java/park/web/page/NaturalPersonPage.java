cf16a91daf2b303a955261945d54ca53895634fb
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2010-03-29 19:34:22 +0000

    Natural Person Edit Page
    git-svn-id: https://construtor.googlecode.com/svn/trunk@471 daf9df78-7b24-0410-bc62-633826f6319b

---
+++ park-jpa/web/src/main/java/park/web/page/NaturalPersonPage.java
@@ -0,0 +1,12 @@
+package park.web.page;
+import org.apache.click.control.Form;
+
+public class NaturalPersonPage extends org.apache.click.Page {
+
+    private Form form = new Form("form");
+
+    public NaturalPersonPage(){
+        addControl(form);
+    }
+
+}
