368ed064eb0233c9338e3e893f25c9d94bb1ce3c
gilbertoca@gmail.com <gilbertoca@gmail.com@daf9df78-7b24-0410-bc62-633826f6319b>
2010-06-15 13:44:33 +0000

    remove @author tags
    git-svn-id: https://construtor.googlecode.com/svn/trunk@533 daf9df78-7b24-0410-bc62-633826f6319b

--- click-jpa-support/src/main/java/org/apache/click/extras/jpa/EntityManagerFilter.java
+++ click-jpa-support/src/main/java/org/apache/click/extras/jpa/EntityManagerFilter.java
@@ -1,17 +1,20 @@
 /*
- * Copyright 2004-2006 Takashi Okamoto, Malcolm A. Edgar
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.click.extras.jpa;
 
@@ -22,7 +25,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.transaction.Transaction;
 
 import java.io.IOException;
 
@@ -55,10 +57,6 @@ import java.io.IOException;
  * 
  * @see EntityManagerContext
  * @see JpaForm
- * 
- * @author Takashi Okamoto
- * @author Malcolm Edgar
- * @author Gilberto Caetano de Andrade
  */
 public class EntityManagerFilter implements Filter {
 
