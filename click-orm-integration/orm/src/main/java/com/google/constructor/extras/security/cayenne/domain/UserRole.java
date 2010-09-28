c982e0dab341861a73198cb1959d7e51db738aef
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2010-09-28 20:54:15 +0000

    No necessary.
    git-svn-id: https://construtor.googlecode.com/svn/trunk@624 daf9df78-7b24-0410-bc62-633826f6319b

--- click-orm-integration/orm/src/main/java/com/google/constructor/extras/security/cayenne/domain/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.google.constructor.extras.security.cayenne.domain;
-
-import com.google.constructor.extras.security.cayenne.domain.auto._UserRole;
-
-public class UserRole extends _UserRole {
-
-}
