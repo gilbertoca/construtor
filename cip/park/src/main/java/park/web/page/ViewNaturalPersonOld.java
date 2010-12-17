73c9e059339bec06b915eb8b366dd509c36936ed
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2010-12-17 17:15:57 +0000

    Cache problem: Click Table or JPA data cache?
    git-svn-id: https://construtor.googlecode.com/svn/trunk@749 daf9df78-7b24-0410-bc62-633826f6319b

---
+++ cip/park/src/main/java/park/web/page/ViewNaturalPersonOld.java
@@ -0,0 +1,87 @@
+package park.web.page;
+
+import com.google.constructor.cip.orm.jpa.BaseJPAService;
+import java.util.List;
+import org.apache.click.control.AbstractLink;
+import org.apache.click.control.ActionLink;
+import org.apache.click.control.Column;
+import org.apache.click.control.PageLink;
+import org.apache.click.control.Table;
+import org.apache.click.dataprovider.DataProvider;
+import org.apache.click.extras.control.LinkDecorator;
+import org.apache.click.extras.control.TableInlinePaginator;
+import org.apache.commons.lang.NotImplementedException;
+import park.model.orm.NaturalPerson;
+
+public class ViewNaturalPersonOld extends park.web.page.BorderPage {
+
+    private static final long serialVersionUID = 1L;
+    protected Table table = new Table("table");
+    protected PageLink editLink = new PageLink("editLink", EditNaturalPerson.class);
+    protected ActionLink deleteLink = new ActionLink("deleteLink", this, "onDeleteClick");
+    private BaseJPAService<NaturalPerson, Long> naturalPersonService;
+
+    // Constructor ------------------------------------------------------------
+    public ViewNaturalPersonOld() {
+        naturalPersonService = new BaseJPAService<NaturalPerson, Long>(NaturalPerson.class);
+
+        getModel().put("title", getMessage("viewNaturalPersonOld.title"));
+        getModel().put("heading", getMessage("viewNaturalPersonOld.heading"));
+        getModel().put("menu", "userMenu");
+
+        addControl(table);
+        addControl(deleteLink);
+        addControl(editLink);
+        //setStateful(true);
+
+        // Setup NaturalPersons table
+        table.setClass(Table.CLASS_COMPLEX);
+        table.setPageSize(10);
+        table.setShowBanner(true);
+        table.setSortable(true);
+        table.setPaginator(new TableInlinePaginator(table));
+        table.setPaginatorAttachment(Table.PAGINATOR_INLINE);
+
+        Column column = new Column("name");
+        table.addColumn(column);
+        column = new Column("legalDocument");
+        table.addColumn(column);
+        column = new Column("dtBirth");
+        column.setFormat("{0,date,dd/MM/yyyy}");
+        table.addColumn(column);
+
+        editLink.setParameter("referrer", "/view-natural-person-old.htm");
+        deleteLink.setAttribute("onclick", getMessage("deleteLink.attribute.onclick"));
+
+        column = new Column("action");
+        column.setTextAlign("center");
+        AbstractLink[] links = new AbstractLink[]{editLink, deleteLink};
+        column.setDecorator(new LinkDecorator(table, links, "id"));
+        column.setSortable(false);
+        table.addColumn(column);
+
+        table.setDataProvider(new DataProvider<NaturalPerson>() {
+
+            public List<NaturalPerson> getData() {
+                return (List<NaturalPerson>) naturalPersonService.getAll();
+            }
+        });
+    }
+
+    // Event Handlers ---------------------------------------------------------
+
+    /**
+     * Handle the delete link click event.
+     *
+     * @return true
+     */
+    public boolean onDeleteClick() {
+        Long id = deleteLink.getValueLong();
+        if (id != null) {
+            naturalPersonService.delete(id);
+        } else {
+            throw new NotImplementedException();
+        }
+        return true;
+    }
+}
