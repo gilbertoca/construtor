4d7f6656924e37c994be383a8e84dd6ea4b76c5c
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2011-06-10 17:22:45 +0000

    accidently removed
    git-svn-id: https://construtor.googlecode.com/svn/trunk@928 daf9df78-7b24-0410-bc62-633826f6319b

---
+++ cip/park-cayenne/src/main/java/park/model/dto/CustomerLookUp.java
@@ -0,0 +1,20 @@
+package park.model.dto;
+
+    public class CustomerLookUp {
+
+        private Long id;
+        private String customerName;
+
+        public CustomerLookUp(Long id, String customerName) {
+            this.id = id;
+            this.customerName = customerName;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getCustomerName() {
+            return customerName;
+        }
+    }
