c3ad38d74c5b0a3e4c9fdfa516d6ef692c6981d2
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2012-06-27 13:18:01 +0000

    EJBLQuery doesn't work with named query
    git-svn-id: https://construtor.googlecode.com/svn/trunk@1042 daf9df78-7b24-0410-bc62-633826f6319b

--- park-samples/park-cayenne/src/test/java/park/model/LegalEntityTest.java
+++ park-samples/park-cayenne/src/test/java/park/model/LegalEntityTest.java
@@ -1,18 +1,16 @@
 package park.model;
 
-import org.apache.cayenne.query.NamedQuery;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Properties;
 import org.apache.cayenne.Cayenne;
 import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.QueryResponse;
 import org.apache.cayenne.access.DataDomain;
 import org.apache.cayenne.access.DataNode;
 import org.apache.cayenne.configuration.server.ServerRuntime;
-import org.apache.cayenne.query.Query;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Properties;
 import org.apache.cayenne.query.EJBQLQuery;
-import org.apache.cayenne.QueryResponse;
+import org.apache.cayenne.query.NamedQuery;
 import org.dbunit.DatabaseUnitException;
 import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
@@ -22,10 +20,10 @@ import org.dbunit.dataset.datatype.IDataTypeFactory;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class LegalEntityTest {
 
@@ -35,13 +33,17 @@ public class LegalEntityTest {
 
     @BeforeClass
     public static void init() throws Exception {
+        //JPA-->emf = Persistence.createEntityManagerFactory("PU");
+        //JPA-->em = emf.createEntityManager();
+        
+        //Create Cayenne ServerRuntime, uses it to get a connection and initializes DBUnit
+        runtime = new ServerRuntime("cayenne-ParkDomain.xml");
+
         // I presume you've set the src/test/resources/jdbc.properties
         Properties configurationProperties = new Properties();
         configurationProperties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("jdbc.properties"));
 
-        //Create Cayenne ServerRuntime, uses it to get a connection and initializes DBUnit
-        runtime = new ServerRuntime("cayenne-ParkDomain.xml");
-
+        //Let's create a new connection to work with DBUnit
         DataDomain domain = runtime.getDataDomain();
         DataNode node = domain.getNode(configurationProperties.getProperty("cayenne.nodeName"));
         connection = new DatabaseConnection(node.getDataSource().getConnection());
@@ -53,7 +55,6 @@ public class LegalEntityTest {
         config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, dataTypeFactory);
         dataset = new FlatXmlDataSetBuilder().build(Thread.currentThread().getContextClassLoader().getResourceAsStream("legal-entity-dataset.xml"));
         DatabaseOperation.CLEAN_INSERT.execute(connection, dataset);
-
     }
 
     @AfterClass
@@ -68,7 +69,7 @@ public class LegalEntityTest {
         DatabaseOperation.REFRESH.execute(connection, dataset);
     }
 
-    @Test
+    //@Test
     public void GetLegalEntityById() {
         System.out.println("\nGetting an Legal Person by ID.\n");
         //LegalEntity lP = em.find(LegalEntity.class, 1002L);
@@ -86,11 +87,14 @@ public class LegalEntityTest {
         //JPA-->del = query.executeUpdate();
         
         ObjectContext context = runtime.getContext();
-        EJBQLQuery query = new EJBQLQuery(new NamedQuery("LegalEntity.deleteById"));
+        //NamedQuery query = new NamedQuery("LegalEntity.deleteById", Collections.singletonMap("id", new Long[]{1010L}));        
+        EJBQLQuery query = new EJBQLQuery("delete from LegalEntity l where l.id = :id");
         query.setParameter("id", 1010L);
         QueryResponse result = context.performGenericQuery(query);
-
+        context.commitChanges();
+        
         int[] count = result.firstUpdateCount();
+        
         assertTrue(count.length > 0);
     }
 }
