095ea764ed65f8bcbe8c08f57e4b2f84f6b02d75
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2013-11-05 16:07:49 +0000

    Removendo artefatos desenvolvidos na secretaria
    git-svn-id: https://construtor.googlecode.com/svn/trunk@1054 daf9df78-7b24-0410-bc62-633826f6319b

--- maven_template_jsf_springdata/src/main/java/br/gov/to/secad/seg/repository/IGrupoRepository.java
+++
@@ -1,9 +0,0 @@
-
-package br.gov.to.secad.seg.repository;
-
-import br.gov.to.secad.seg.domain.Grupo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional(readOnly = true)
-public interface IGrupoRepository extends JpaRepository<Grupo, Integer> { }
