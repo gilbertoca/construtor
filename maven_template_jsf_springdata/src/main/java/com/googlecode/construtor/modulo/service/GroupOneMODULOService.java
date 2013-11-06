a8e50fa02112e017d9d5db714219c1b0e027dc04
gilbertoca <gilbertoca@daf9df78-7b24-0410-bc62-633826f6319b>
2013-11-06 21:45:13 +0000

    Primeiro managedbean
    git-svn-id: https://construtor.googlecode.com/svn/trunk@1060 daf9df78-7b24-0410-bc62-633826f6319b

--- maven_template_jsf_springdata/src/main/java/com/googlecode/construtor/modulo/service/GroupOneMODULOService.java
+++ maven_template_jsf_springdata/src/main/java/com/googlecode/construtor/modulo/service/GroupOneMODULOService.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Service;
 
 import com.googlecode.construtor.modulo.domain.EstadoCivil;
 import com.googlecode.construtor.modulo.repository.IEstadoCivilRepository;
+import java.util.Arrays;
 
 /**
  * Classe agrupamento de serviços. Aqui agruparemos os serviços referentes às
- * seguintes classes de domínio: Municipio, Logradouro, Nacionalidade, Escolaridade,
- * EstadoCivil.
+ * seguintes classes de domínio: Municipio, Logradouro, Nacionalidade,
+ * Escolaridade, EstadoCivil.
  * <p/>
  * @author gilberto
  */
@@ -21,21 +22,30 @@ public class GroupOneMODULOService {
 
     @Autowired
     private IEstadoCivilRepository estadoCivilRepository;
+    static {
+        List<EstadoCivil> scList = Arrays.asList(new EstadoCivil(1, "CASADO"), new EstadoCivil(2, "SOLTEIRO"), new EstadoCivil(3, "DIVORCIADO"));
+        estadoCivilRepository.save(scList);
+    }
     
     public GroupOneMODULOService() {
     }
-    
+
     public String getDateFormat() {
-    	return "dd/MM/yyyy";
+        return "dd/MM/yyyy";
     }
-    
+
     //EstadoCivil
-    public List<EstadoCivil> findAllEstadoCivil() {return estadoCivilRepository.findAll();}
-    public EstadoCivil findOneEstadoCivil(Integer id){return estadoCivilRepository.findOne(id);}    
+    public List<EstadoCivil> findAllEstadoCivil() {
+        return estadoCivilRepository.findAll();
+    }
+
+    public EstadoCivil findOneEstadoCivil(Integer id) {
+        return estadoCivilRepository.findOne(id);
+    }
 
     //Hoje
     public Date getHoje() {
-    	return new Date(System.currentTimeMillis());
+        return new Date(System.currentTimeMillis());
     }
-    
+
 }
---
+++ maven_template_jsf_springdata/src/main/java/com/googlecode/construtor/modulo/view/EstadoCivilPage.java
@@ -0,0 +1,55 @@
+package com.googlecode.construtor.modulo.view;
+
+import com.googlecode.construtor.modulo.domain.EstadoCivil;
+import com.googlecode.construtor.modulo.service.GroupOneMODULOService;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author gilberto.andrade
+ */
+@ManagedBean
+@ViewScoped
+public class EstadoCivilPage {
+    @ManagedProperty("#{groupOneMODULOService}")
+    private GroupOneMODULOService groupOneMODULOService;
+
+    private int selectedId;
+    private EstadoCivil selectedEstadoCivil;
+
+    public List<EstadoCivil> getEstadoCivis() {
+        return groupOneMODULOService.findAllEstadoCivil();
+    }
+
+    public void loadEstadoCivil() {
+        selectedEstadoCivil = groupOneMODULOService.findOneEstadoCivil(selectedId);
+    }
+
+    public void addInfoMessage() {
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        String msg = "View action " + (ctx.isPostback() ? "on postback" : "on initial request");
+        ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null));
+        ctx.getExternalContext().getFlash().setKeepMessages(true);
+    }
+
+    public EstadoCivil getSelectedEstadoCivil() {
+        return selectedEstadoCivil;
+    }
+
+    public void setSelectedId(int selectedId) {
+        this.selectedId = selectedId;
+    }
+
+    public int getSelectedId() {
+        return selectedId;
+    }
+
+    public void setGroupOneMODULOService(GroupOneMODULOService groupOneMODULOService) {
+        this.groupOneMODULOService = groupOneMODULOService;
+    }
+}
